3ad9f2ba3612c1ce0282c7018b2626f9c65d1ae6
edan_3000 <roseswallow13@gmail.com>
2023-06-30 14:32:08 +0900

    feat: BaseBallApp make method

--- src/main/java/com/baseballmanager/bmapp/BaseBallApp.java
+++ src/main/java/com/baseballmanager/bmapp/BaseBallApp.java
@@ -7,28 +7,34 @@ import java.util.Scanner;
 
 public class BaseBallApp {
 
-	public static void main(String[] args) {
-		appStart();
+	public static BaseBallApp baseBallApp;
+	public static BaseBallApp getInstance() {
+		if (baseBallApp == null) {
+			baseBallApp = new BaseBallApp();
+		}
+		return baseBallApp;
 	}
-
-	public static void appStart() {
+	public static void details() {
+		System.out.println("\n어떤 기능을 요청하시겠습니까?");
+		System.out.println("기능 (ex : 선수등록?teamId=1&name=이대호&position=1루수)");
+		System.out.println("프로그램 종료(END)");
+	}
+	public static void start() {
+		details();
 		BaseBallView baseBallView = new BaseBallView();
 		Scanner sc = new Scanner(System.in);
 
 		while (true) {
-			System.out.println("\n어떤 기능을 요청하시겠습니까?");
-			System.out.println("야구장등록 OR 야구장목록 : ");
-			System.out.println("팀등록 OR 팀목록 : ");
-			System.out.println("선수등록 OR 선수목록 : ");
-			System.out.println("END : 프로그램 종료");
 			String input = sc.nextLine();
 			if (input.equals("END") || input.equals("end")) {
-				System.out.println("\n프로그램을 종료합니다.");
 				break;
 			}
 
+			String[] restAPI = input.split("\\?");
+			String command = restAPI[0];
+
 
 		}
+		System.out.println("프로그램을 종료합니다.");
 	}
-
 }
